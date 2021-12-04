fad341f4dfb01ef2980efdbf94cfd54b8a25e77c
greatmomo <66915435+greatmomo@users.noreply.github.com>
2021-12-04 17:30:33 +0100

    black and white works

--- script.js
+++ script.js
@@ -18,7 +18,32 @@ function generateCells(cells) {
         for (let i = 0; i < cells; i++) {
             let newDiv = document.createElement('div');
             newDiv.className = 'cell-row';
+            newDiv.addEventListener('mouseover', sketch);
             column.appendChild(newDiv);
         }
     });
 }
+
+// create a function that changes the color of the div on 'hover'
+function sketch(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+function resetSketch() {
+    // delete divs
+    const elements = document.querySelectorAll('.cell-column');
+    elements.forEach(element => {
+        element.parentNode.removeChild(element);
+    })
+
+    // prompt new size
+    let newSize = 0;
+    while (newSize < 1 || newSize > 100) {
+        newSize = prompt('Please enter a new size (1-100):');
+    }
+
+    generateCells(newSize);
+}
+
+generateCells(16);
+document.querySelector('.button-reset').addEventListener('click', resetSketch);
